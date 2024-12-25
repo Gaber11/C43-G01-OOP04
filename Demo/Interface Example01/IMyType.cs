cb0cb51590d606986a5a8cf54d1edf0e60da9edf
Gaber11 <gaberezzat0@gmail.com>
2024-12-25 17:25:06 +0200

    Part 02 Interface - Example 01

---
+++ Demo/Interface Example01/IMyType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Interface_Example01
+{
+    internal interface IMyType
+    {
+        //Defualt Access Modifier inside interface is "Public"
+        //Private Access Modifier is not allowed inside the interface for Signature
+        //What you can write inside the Interface?
+
+        //1. Signature of Property.
+        int MyProperty { get; set; }
+
+        //2.  Signature of Method 
+         void MyFunction();
+
+        //3. Defualt Implemented Method allow [private] .
+         void Print()
+        {
+            Console.WriteLine("Hello Defualt Implemnted Method :) ");
+        }
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace Demo
+﻿using Demo.Interface_Example01;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part 02 Interface - Example 01
+           // IMyType myType;
+            //Declare for reference from type "ImyType" , containing Garbag Value
+            //CLR will allocate 4 bytes in the stack .
+            //CLR will allocate 0 bytes in the Heap .
+
+            //myType = new IMyType(); //invalid ==> U Can't Create an object from interface
+
+           // MyType myType = new MyType();
+           // myType.MyProperty = 10;
+           // myType.MyFunction();
+           //// myType.Print();
+           //IMyType refFromInterface = new MyType();
+           // refFromInterface.MyFunction();
+           // refFromInterface.Print();
+
+            #endregion
+
         }
     }
 }
