c91230ce3943e677fe481e6cd54b317e328d1a71
Gaber11 <gaberezzat0@gmail.com>
2024-12-27 05:13:01 +0200

    Part01: Question 01

---
+++ Assignment/InterFaces/BasicAuthenticationService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.InterFaces
+{
+    internal class BasicAuthenticationService : IAuthenticationService
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public BasicAuthenticationService(string _username="Admin" , string _password = "joker4444")
+        {
+            username= _username;
+            password= _password;
+        }
+        public bool AuthenticateUser()
+        {
+            if (username == "Admin" && password == "joker444")
+            {
+                return true;
+            }
+            return false;
+        }
+       
+
+        public bool AuthorizeUser()
+        {
+            if (username is not null)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using Assignment.InterFaces;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Part01: Question 01
+
+            //IAuthenticationService basicAuthenticationService = new BasicAuthenticationService()
+            //{
+            //    username = "Admin",
+            //    password = "joker444",
+            //};
+
+            //Console.WriteLine(basicAuthenticationService.AuthenticateUser());
+
+
+
+
+
+            #endregion
+        }
+    }
+}
